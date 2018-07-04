963c335971a73d2cacd96c26289159111d427d40
LAPTOP-3NA0PAG2\Jay Modi <jay.modi58@gmail.com>
2018-07-04 19:30:17 -0400

    Added the source code of currency coversion service.

--- currency-conversion-service/src/main/java/com/currencyconversion/currencyconversionservice/CurrencyConversionBean.java
+++ currency-conversion-service/src/main/java/com/currencyconversion/currencyconversionservice/CurrencyConversionBean.java
@@ -1,37 +1,31 @@
-package com.forex.forexservice;
+package com.currencyconversion.currencyconversionservice;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 
-@Entity
-public class ExchangeValue {
+public class CurrencyConversionBean {
 
-	@Id
 	private Long id;
-
-	@Column(name = "currency_from")
 	private String from;
-
-	@Column(name = "currency_to")
 	private String to;
-
-	// Auto binding by name
 	private BigDecimal conversionMultiple;
-
-	// Auto binding by name
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
 	private int port;
 
-	public ExchangeValue() {
+	public CurrencyConversionBean() {
+		super();
 	}
 
-	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+	public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, int port) {
 		super();
 		this.id = id;
 		this.from = from;
 		this.to = to;
 		this.conversionMultiple = conversionMultiple;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.port = port;
 	}
 
 	public Long getId() {
@@ -64,6 +58,22 @@ public class ExchangeValue {
 
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
 	}
 
 	public int getPort() {
