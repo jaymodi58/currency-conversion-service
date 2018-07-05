185e0f7fa8791cec708e3eb5ad7fb0b7d1adb0b2
LAPTOP-3NA0PAG2\Jay Modi <jay.modi58@gmail.com>
2018-07-05 14:19:27 -0400

    Added Eureka Naming Server.

--- currency-conversion-service/src/main/java/com/currencyconversion/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/currencyconversion/currencyconversionservice/CurrencyConversionController.java
@@ -1,25 +1,23 @@
 package com.currencyconversion.currencyconversionservice;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	// Without using Feign client
-	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	/*
+	 * Without using Feign client
+	 */
+	/*@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
@@ -35,13 +33,16 @@ public class CurrencyConversionController {
 
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
-	}
+	}*/
 
-	// Using Feign client
+	/*
+	 * Using Feign client
+	 */
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
 
-	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	// @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
